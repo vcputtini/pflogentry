f7382ef76f5150865bb3a22e429c08dd297267f0
Volnei C Puttini <vcputtini@gmail.com>
2021-12-08 15:18:18 -0300

    First commit

---
+++ CMakeFiles/pflogentry.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pflogentry.dir/pflogentry.cc.o"
+  "CMakeFiles/pflogentry.dir/pflogentry.cc.o.d"
+  "libpflogentry.pdb"
+  "libpflogentry.so"
+  "libpflogentry.so.1"
+  "libpflogentry.so.1.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pflogentry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
