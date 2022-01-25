65cd6ce514297488fd63e877a4cc9b36b3c92cc4
Volnei C Puttini <vcputtini@gmail.com>
2022-01-25 13:58:22 -0300

    Update examples/ex-rawtoxml.cc
    Now allows you to select a specific date/time range for conversion.

--- examples/ex-rawtoxml.cc
+++ examples/ex-rawtoxml.cc
@@ -22,10 +22,22 @@ main()
    * format does not include this information in the log line.
    * For the SysLog format (RFC-5424) this is not necessary. */
 
+
+/*
+ * LOG_FORMAT == 0  -> LogSyslog
+ * LOG_FORMAT == 1  -> Logbsd
+ */ 
+#define LOG_FORMAT 0
+  
   std::cout << "Loading ...\n";
   PFLogentry::PFLError err;
-  // PFLogentry *pfl = new PFLogentry(PFLogentry::LogSyslog);
+  
+#if LOG_FORMAT   
   PFLogentry* pfl = new PFLogentry(LogFormat::LogBSD, 2021);
+#else  
+  PFLogentry *pfl = new PFLogentry(LogFormat::LogSyslog);
+#endif  
+  
   while (std::getline(std::cin, raw_log)) {
     pfl->append(raw_log);
     if (pfl->errorNum() != PFLError::PFL_SUCCESS) {
@@ -37,11 +49,25 @@ main()
   std::cout << "Load size = " << pfl->size() << "\n";
   
   /*
-   * PFLError toXML(const std::string fn_);
+   * PFLError toXML(const std::string&& fn_,
+                  const std::string&& d0_,
+                  const std::string&& t0_,
+                  const std::string&& d1_,
+                  const std::string&& t1_);
    */
   // Warning: all data in memory is erased after this operation.
   std::cout << "Converting ... \n";
+  
+  // All log entries:
   err = pfl->toXML("filter-log.xml");
+  
+  // Possible operations:
+  // err = pfl->toXML("filter-log.xml","yyyy-mm-dd","hh:mm:ss","yyyy-mm-dd","hh:mm:ss");
+  // All day:
+  // err = pfl->toXML("filter-log.xml","2022-01-25","00:00:00","2022-01-25","23:59:59");
+  // Time range:
+  // err = pfl->toXML("filter-log.xml","2022-01-25","01:00:00","2022-01-25","02:30:00");
+  
   if (err != PFLError::PFL_SUCCESS) {
     std::cout << static_cast<int>(pfl->errorNum()) << " :  "
     << pfl->getErrorText() << "\n";
